79ddcc48073e967bf05430c94cf68d08ac0c956b
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-19 16:52:36 -0500

    Add MVV controller with views using EF

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Web.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebContext") ?? throw new InvalidOperationException("Connection string 'WebContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
