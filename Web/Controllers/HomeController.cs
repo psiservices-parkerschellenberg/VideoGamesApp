1e860c408b849b7eb8cda531f29e1084aa70c2e7
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-20 08:30:47 -0500

    Remove Home Data view

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -18,10 +18,10 @@ namespace Web.Controllers
             return View();
         }
 
-        public IActionResult Data()
-        {
-            return View();
-        }
+        //public IActionResult Data()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
