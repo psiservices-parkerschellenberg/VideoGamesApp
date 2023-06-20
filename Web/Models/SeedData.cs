cff9be032eae0b7c4e63ac4fef580003c027fbe1
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-20 10:26:51 -0500

    Add SeedData

---
+++ Web/Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Web.Data;
+using System;
+using System.Linq;
+
+namespace Web.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new WebContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<WebContext>>()))
+        {
+            // Look for any Games.
+            if (context.Game.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Game.AddRange(
+                new Game
+                {
+                    Title = "Mario Kart",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Developer = "Nintendo",
+                    Price = 7.99M
+                },
+                new Game
+                {
+                    Title = "Grand Theft Auto V",
+                    ReleaseDate = DateTime.Parse("2012-3-13"),
+                    Developer = "Rockstar",
+                    Price = 28.99M
+                },
+                new Game
+                {
+                    Title = "Need For Speed Underground",
+                    ReleaseDate = DateTime.Parse("2003-2-23"),
+                    Developer = "EA",
+                    Price = 9.99M
+                },
+                new Game
+                {
+                    Title = "Need For Speed Underground 2",
+                    ReleaseDate = DateTime.Parse("2004-4-15"),
+                    Developer = "EA",
+                    Price = 23.99M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Web.Data;
+using Web.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<WebContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WebContext") ?? throw new InvalidOperationException("Connection string 'WebContext' not found.")));
@@ -9,6 +11,13 @@ builder.Services.AddDbContext<WebContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
