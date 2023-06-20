4b7f7e3598646099a2b6fbe41bc048a10d6e84a9
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-20 10:41:18 -0500

    Add data annotations

--- Web/Models/Game.cs
+++ Web/Models/Game.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Web.Models
 {
@@ -6,9 +8,11 @@ namespace Web.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Developer { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
